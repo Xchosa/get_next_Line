f4aeea6283aa10b43dc30a366e3fef5c112a1797
Paul Overbeck <poverbec@2-F-9.42heilbronn.de>
2024-11-18 11:37:10 +0100

    seg fault, if read = 0

--- get_next_line.c
+++ get_next_line.c
@@ -6,81 +6,87 @@
 /*   By: poverbec <poverbec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 09:15:48 by poverbec          #+#    #+#             */
-/*   Updated: 2024/11/15 14:28:05 by poverbec         ###   ########.fr       */
+/*   Updated: 2024/11/18 11:31:40 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./get_next_line.h"
 
+// nicht pushen. eigens backup funktioniert
+// pointer in read_store noch falsch , wann &
+// last copilot call explain why **line 
 static char* ft_createline(char *line);
+static int read_store(char *buffer, char **line, int fd, int flag);
+static void move_buffer_forward(char *buffer);
 
 char	*get_next_line(int fd)
 {
-	ssize_t		bytes_read;
 	static char	buffer[BUFFER_SIZE+1];
-	char 		*line; // output
+	char 		*line;
 	char		*tmp_buffer;
-	size_t		i;
-	size_t		j;
+	int			flag;
+
+	flag = 0;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = ft_strjoin("", buffer);
 	if(!line)
 		return(NULL);
-	// hat nichts geaendert
-	// line = tmp_buffer;
-	// free(tmp_buffer);
-	while(!ft_strchr(buffer, '\n') )// buffer[0] is not empty
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if(bytes_read == 0)
-		{
-			if (buffer[0] == 0)
-				return (free(line), NULL);
-			ft_bzero(buffer, BUFFER_SIZE +1);
-			// tmp_buffer = line;
-			return(line);
-		}
-		else if (bytes_read < 0)
-		{
-			ft_bzero(buffer, BUFFER_SIZE +1);
-			return (free(line), line = NULL, NULL);
-		}
-		buffer[bytes_read] = '\0';
-		tmp_buffer = ft_strjoin(line, buffer);
-		if (!tmp_buffer)
-			return(free(line), line = NULL, NULL);
-		free(line);
-		line = tmp_buffer;
+	if (!ft_strchr(buffer, '\n'))
+		if (read_store(buffer, &line,fd, flag) < 0)
+			return (NULL);
+	 if (flag == 1)
+	 	return (line);
 
-	}
 	tmp_buffer = ft_createline(line);
 	if (!tmp_buffer)
-		return (NULL);
+		return (free(line), NULL);
 	free(line);
-	i = 0;
-	while(buffer[i] != '\n' && buffer[i])
-		i++;
-	j = 0;
-	while (buffer[i])
-	{
-		buffer[j] = buffer[i + 1];
-		j++;
-		i++;
-	}
-	buffer[j] = '\0';
+	move_buffer_forward(buffer);
 	return(tmp_buffer);
 }
+
+static int read_store(char *buffer, char **line, int fd, int flag)
+{
+	ssize_t bytes_read;
+	char	*tmp_buffer;
+	
+		while(!ft_strchr(buffer, '\n'))
+		{
+			bytes_read = read(fd, buffer, BUFFER_SIZE);
+			if(bytes_read == 0)
+			{
+				if (buffer[0] == 0)
+					return (0);
+					return(ft_bzero(buffer, BUFFER_SIZE +1), flag = 1 , 0);// should return directly line. 
+			}
+			else if (bytes_read < 0)
+			{
+				ft_bzero(buffer, BUFFER_SIZE +1);
+				return (free(*line), line = NULL, -1);
+			}
+			buffer[bytes_read] = '\0';
+			tmp_buffer = ft_strjoin(*line, buffer);
+			if (!tmp_buffer)
+				return(free(line), line = NULL, -1);
+			free(*line);
+			*line = tmp_buffer;
+		}
+	return(0);
+}
+
+
 char *ft_createline(char *line)
 {
 	char *newline;
 	ssize_t i;
 	i = 0;
-	while(line[i]!= '\n')
-	{
+	while(line[i]!= '\n' )
 		i++;
-	} 
+	// 	if (line [i] == '\n')
+	// 		break ;
+	// while()
 	i++;
 	newline = (char*) malloc (i * sizeof(char) + 1);
 	if(!newline)
@@ -89,31 +95,48 @@ char *ft_createline(char *line)
 	newline[i] = '\0';
 	return(newline);
 }
+static void move_buffer_forward(char *buffer)
+{
+	size_t i;
+	size_t j;
+	
+	i = 0;
+	j = 0;
+	while(buffer[i] != '\n' && buffer[i])
+		i++;
+	while (buffer[i])
+	{
+		buffer[j] = buffer[i + 1];
+		j++;
+		i++;
+	}
+	buffer[j] = '\0';
+}
 	
 
 	// wenn wir new line haben dann returnen und buffer updaten. 
 
-// int	main(void)
-// {
-// 	int fd;
-// 	char *test = NULL;
+int	main(void)
+{
+	int fd;
+	char *test = NULL;
 	
-// 	fd = open("poem.txt", O_RDONLY);
-// 	if (fd == -1)
-// 	{
-// 		printf("error in reopening poem");
-// 		return (1);
-// 	}
+	fd = open("poem.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("error in reopening poem");
+		return (1);
+	}
 
-// 	while(1)
-// 	{
-// 		test = get_next_line(fd);
-// 		if (!test)
-// 			break;
-// 		printf("%s", test);
-// 	}
+	while(1)
+	{
+		test = get_next_line(fd);
+		if (!test)
+			break;
+		printf("%s", test);
+	}
 
-// 	// free(test);
-// 	close(fd);
-// 	return (0);
-// }
+	free(test);
+	close(fd);
+	return (0);
+}
